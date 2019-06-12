a4d0940784ae35a35ec450580d399533b4ab6745
Tyler Hunt <tyhunt04@gmail.com>
2019-06-12 18:12:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function produceDrivingRange(range) {
   return function(start, end) {
 
-    let tripDistance = (parseInt(end) - parseInt(end));
+    let tripDistance = (parseInt(end) - parseInt(start));
     let difference = (range - tripDistance);
     if (tripDistance <= range) {
       return `within range by ${tripDistance}`;
